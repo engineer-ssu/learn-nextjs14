cefc2074125f94d6757bb2c90419cb7dcec378dc
suyeon <developer.ssu.ssu@gmail.com>
2024-06-12 21:09:03 +0900

    feat : original 통신

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,25 @@
-export const metadata = {
-    title : "Home"
-}
+"use client";
+
+import { useEffect, useState } from "react"
+
 export default function Page () {
+    const [movies, setMovies] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getMovies = async ()=> {
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+
     return <div>
         <h1>Hello!</h1>
+        {isLoading ? <>로딩</> : JSON.stringify(movies)}
     </div>
 }
